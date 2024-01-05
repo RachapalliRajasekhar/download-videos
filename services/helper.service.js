05e6f39703162295fc0c701c06614f65d4a2803e
rajasekhar <rajasekhar.rachapalli@falkondata.com>
2024-01-06 00:21:02 +0530

    commnt

--- services/helper.service.js
+++ services/helper.service.js
@@ -30,7 +30,7 @@ function printSuccess(message) {
 
 function printError(message) {
   console.error(errorChalk.bold(`${getCurrentDateTimeIST()}:- ${message}`));
-  throw error;
+  throw message;
 }
 
 function printWarning(message) {
